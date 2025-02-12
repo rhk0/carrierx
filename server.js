bebaa43d9cfe69c8e643fbdbbd79c16943686dde
rahul <rhk0012@gmail.com>
2025-02-12 20:09:07 +0530

    fhas

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
 import path from "path";  // Import path module to handle file paths
+
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 
@@ -11,21 +12,14 @@ connectDB();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-// API Routes
 app.use("/api/auth", authRoutes);
 
-// Serve static files from the React build directory
-app.use(express.static(path.join(__dirname, 'build')));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-// Catch-all handler for React routes
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
